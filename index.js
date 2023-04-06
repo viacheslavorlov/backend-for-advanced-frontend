6f9537e1acaf2dc3b4f84fa6a078cc7c65d81bba
Viacheslav Orlov <viacheslavorlov@gmail.com>
2023-04-06 23:18:22 +0300

    rewriter for db.json

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
+// See https://github.com/typicode/json-server#module
+
+
+
+// Add this before server.use(router)
+
+server.use(router)
+server.listen(3000, () => {
+    console.log('JSON Server is running')
+})
+
+// Export the Server API
+module.exports = server
+
+const jsonServer = require('json-server')
+const middlewares = jsonServer.defaults()
 const fs = require('fs');
-const jsonServer = require('json-server');
 const path = require('path');
 
 const server = jsonServer.create();
+server.use(jsonServer.rewriter({
+    '/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
+server.use(middlewares)
 
 const router = jsonServer.router(path.resolve(__dirname, 'db.json'));
 
