78aaf6254c76ef16c29bf7137dc69437f1cd586c
Viacheslav Orlov <viacheslavorlov@gmail.com>
2023-06-18 13:08:24 +0300

    db updatable

--- index.js
+++ index.js
@@ -52,6 +52,6 @@ server.use((req, res, next) => {
 server.use(router);
 
 // запуск сервера
-server.listen(8000, () => {
-    console.log('server is running on 8000 port');
+server.listen(process.env.PORT || 8000, () => {
+    console.log('server is running');
 });
