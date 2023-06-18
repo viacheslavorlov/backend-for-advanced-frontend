0b7a897a1111e0de52df3d6cd49db6809bc224ec
Viacheslav Orlov <viacheslavorlov@gmail.com>
2023-06-18 14:07:53 +0300

    db updatable

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const fs = require("fs");
-const dbFile = "db.json";
-function readDB() {
-	return JSON.parse(fs.readFileSync(dbFile, "utf-8"));
-}
-function writeDB(data) {
-	fs.writeFileSync(dbFile, JSON.stringify(data, null, 2));
-}
-// авторизация для пользователя
-app.post('/login', (req, res) => {
+const jsonServer = require("json-server");
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+app.use("/api", middlewares);
+app.use("/api", router);
+
+// Эндпоинт для логина
+server.post('/login', (req, res) => {
 	try {
 		const { username, password } = req.body;
 		const db = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'db.json'), 'UTF-8'));
@@ -24,94 +22,26 @@ app.post('/login', (req, res) => {
 			return res.json(userFromBd);
 		}
 		
-		let errorMessage = 'User not found';
-		
-		return res.status(403).json({ message: errorMessage });
+		return res.status(403).json({ message: 'User not found' });
 	} catch (e) {
 		console.log(e);
-		return res.status(500).json({ message: 'Произошла ошибка: ' + e.message });
+		return res.status(500).json({ message: e.message });
 	}
 });
 
+// проверяем, авторизован ли пользователь
+// eslint-disable-next-line
+app.use((req, res, next) => {
+	if (!req.headers.authorization) {
+		return res.status(403).json({ message: 'AUTH ERROR' });
+	}
+	
+	next();
+});
 
-// Работа с уведомлениями
-app.get("/api/notifications", (req, res) => {
-	const db = readDB();
-	res.json(db.notifications);
-});
-app.post("/api/notifications", (req, res) => {
-	const db = readDB();
-	const newNotification = req.body;
-	newNotification.id = Date.now().toString();
-	db.notifications.push(newNotification);
-	writeDB(db);
-	res.json(newNotification);
-});
-// Работа со статьями
-app.get("/api/articles", (req, res) => {
-	const db = readDB();
-	res.json(db.articles);
-});
-app.post("/api/articles", (req, res) => {
-	const db = readDB();
-	const newArticle = req.body;
-	newArticle.id = Date.now().toString();
-	db.articles.push(newArticle);
-	writeDB(db);
-	res.json(newArticle);
-});
-// Работа с комментариями
-app.get("/api/comments", (req, res) => {
-	const db = readDB();
-	res.json(db.comments);
-});
-app.post("/api/comments", (req, res) => {
-	const db = readDB();
-	const newComment = req.body;
-	newComment.id = Date.now().toString();
-	db.comments.push(newComment);
-	writeDB(db);
-	res.json(newComment);
-});
-// Работа с пользователями
-app.get("/api/users", (req, res) => {
-	const db = readDB();
-	res.json(db.users);
-});
-app.post("/api/users", (req, res) => {
-	const db = readDB();
-	const newUser = req.body;
-	newUser.id = Date.now().toString();
-	db.users.push(newUser);
-	writeDB(db);
-	res.json(newUser);
-});
-// Работа с профилями
-app.get("/api/profile", (req, res) => {
-	const db = readDB();
-	res.json(db.profile);
-});
-app.post("/api/profile", (req, res) => {
-	const db = readDB();
-	const newProfile = req.body;
-	newProfile.id = Date.now().toString();
-	db.profile.push(newProfile);
-	writeDB(db);
-	res.json(newProfile);
-});
-// Работа с рейтингами статей
-app.get("/api/article-ratings", (req, res) => {
-	const db = readDB();
-	res.json(db["article-ratings"]);
-});
-app.post("/api/article-ratings", (req, res) => {
-	const db = readDB();
-	const newRating = req.body;
-	newRating.id = Date.now().toString();
-	db["article-ratings"].push(newRating);
-	writeDB(db);
-	res.json(newRating);
-});
-app.listen(port, () => {
+app.use(router);
+
+	
+	app.listen(port, () => {
 	console.log(`Server is running at http://localhost:${port}`);
 });
